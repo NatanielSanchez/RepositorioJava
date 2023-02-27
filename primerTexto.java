661ce6524213d88f4fe373a4992dcd9445ee2eed
NatanielSanchez <natanielsanchez2077@gmail.com>
2023-02-27 20:20:36 -0300

    Update primerTexto.java

--- primerTexto.java
+++ primerTexto.java
@@ -2,7 +2,7 @@ public class primerTexto
 {
 	public static void main(String[] args)
 	{
-		System.out.println("HOLA MUNDO!!!")
-		System.out.println("otro print")
+		System.out.println("HOLA MUNDO!!!");
+		System.out.println("otro print");
 	}
 }
